858b883cca267da3f6fb54976e6451757593421f
wagner <wagner_mba@hotmail.com>
2017-11-28 21:07:10 -0200

    commit alterações finais

--- DesafioATG1/app/view2/view2.js
+++ DesafioATG1/app/view2/view2.js
@@ -2,15 +2,15 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
     .controller('View2Ctrl', ['$scope', function ($scope) {
-        $scope.valor = 0;
+        $scope.valor = 100;
         $scope.moedaInicial = 'USD';
         $scope.moedaFinal = 'USD';
         function loadJSON(path, success, error) {
@@ -33,15 +33,13 @@ angular.module('myApp.view2', ['ngRoute'])
         $scope.converter = function (de, para, valor) {
             loadJSON('https://forex.1forge.com/1.0.2/convert?from=' + de + '&to=' + para + '&quantity=' + valor + '&api_key=eBQyTZiqrgLVPSFHWCDpJ23tepFYnEXt',
                 function (data) {
-                    $scope.Lista = data;
-                    $scope.coisa = true;
+                    $scope.resultado = data.text;
                     console.log(data);
-                    setTimeout($scope.getCotacao, 30000);
-                    $scope.apply;
+                    $scope.$apply();
                 },
                 function (xhr) {
                     console.error(xhr);
                 }
             );
         }
-}]);
+    }]);
